23e569bfbdd79f15761be0659b297c3af4165175
Solskin <cph-mm712@cphbusiness.dk>
2023-02-01 16:09:10 +0100

    adds tomcat plugin in pom

--- src/test/java/Facade/DBFacadeTest.java
+++ src/test/java/Facade/DBFacadeTest.java
@@ -50,15 +50,14 @@ class DBFacadeTest {
     }
 
     @Test
-    public void test() {
+    public void test()throws SQLException {
         System.out.println("Testing database conncetion... ");
         String  sql = "SELECT * FROM startcode_test.usertable";
         try (ResultSet set = con.prepareStatement(sql).executeQuery()){
             set.next();
             String name = set.getString("fname");
             assertEquals("Hans", name);
-            } catch (SQLException throwables) {
-            throwables.printStackTrace();
+
         }
     }
-    }
+}
